fbfb8f770b275788892daac9d48360ac6ce3577c
palmix <qrrq.refai@gmail.com>
2020-05-10 03:00:27 +0300

    jsc

---
+++ jsc/js/util/uglify-js-browser.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/no-unresolved, import/extensions, import/no-webpack-loader-syntax
+import UglifyJSBrowser from 'exports-loader?UglifyJS!uglify-js/uglify-js-browser';
+
+export default UglifyJSBrowser;
