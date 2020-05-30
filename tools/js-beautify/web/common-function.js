b78c14a48af9c17ab92ac80b2830b594ebdfeb5b
palmix <qrrq.refai@gmail.com>
2020-05-30 06:56:35 +0300

    Update common-function.js

--- tools/js-beautify/web/common-function.js
+++ tools/js-beautify/web/common-function.js
@@ -136,6 +136,24 @@ function remove_cookie() {
   Cookies.set('language', $('#language').val(), opts);
   Cookies.set('indent-empty-lines', $('#indent-empty-lines').prop('checked') ? 'on' : 'off', opts);
 
+  $('#tabsize').val(any(Cookies.get('tabsize'), '4'));
+  $('#brace-style').val(any(Cookies.get('brace-style'), 'collapse'));
+  $('#detect-packers').prop('checked', Cookies.get('detect-packers') !== 'off');
+  $('#max-preserve-newlines').val(any(Cookies.get('max-preserve-newlines'), '5'));
+  $('#keep-array-indentation').prop('checked', Cookies.get('keep-array-indentation') === 'on');
+  $('#break-chained-methods').prop('checked', Cookies.get('break-chained-methods') === 'on');
+  $('#indent-scripts').val(any(Cookies.get('indent-scripts'), 'normal'));
+  $('#additional-options').val(any(Cookies.get('additional-options'), '{}'));
+  $('#space-before-conditional').prop('checked', Cookies.get('space-before-conditional') !== 'off');
+  $('#wrap-line-length').val(any(Cookies.get('wrap-line-length'), '0'));
+  $('#unescape-strings').prop('checked', Cookies.get('unescape-strings') === 'on');
+  $('#jslint-happy').prop('checked', Cookies.get('jslint-happy') === 'on');
+  $('#end-with-newline').prop('checked', Cookies.get('end-with-newline') === 'on');
+  $('#indent-inner-html').prop('checked', Cookies.get('indent-inner-html') === 'on');
+  $('#comma-first').prop('checked', Cookies.get('comma-first') === 'on');
+  $('#e4x').prop('checked', Cookies.get('e4x') === 'on');
+  $('#language').val(any(Cookies.get('language'), 'html'));
+  $('#indent-empty-lines').prop('checked', Cookies.get('indent-empty-lines') === 'on');
 }
 
 
