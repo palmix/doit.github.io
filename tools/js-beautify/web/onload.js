db10e5f972d75a978a1d1f84dfb13a10bab140b0
palmix <qrrq.refai@gmail.com>
2020-05-30 03:59:58 +0300

    Update onload.js

--- tools/js-beautify/web/onload.js
+++ tools/js-beautify/web/onload.js
@@ -6,10 +6,10 @@ $(function() {
   $.getJSON("https://get.doitf.com/tools/js-beautify/package.json", function(data) {
     $('#version-number').text('(v' + data.version + ')');
   });
-  var default_html="<!--هذا مجرد نموذج للرمز البرمجي، إلصق الكود الحقيقي \n يمكنك تغيير نوع الرمز البرمجي من أعلى هذا الصندوق .-->\n\n<!DOCTYPE html> <html dir='rtl'> <head> <meta charset='utf-8'>\n\n<title>موقع أدوات ويب</title></head><body><h1>كيف يمكن إعادة ترتيب ملفات html </h1>\n<p>أداة إعادة ترتيب الرموز البرمجية لمحتويات الويب</p> <p id='demo'>\n\n</p><script> var text = 'إختبار إعادة تنظيم الملفات'; document.getElementById('demo').innerHTML = text;</script></body></html>";
-var default_css = "/*===\nهذا مجرد نموذج للرمز البرمجي، إلصق الكود الحقيقي \n يمكنك تغيير نوع الرمز البرمجي من أعلى هذا الصندوق. \n===*/\n\n#adawatweb{border: 1px solid #eee;height:auto;padding:15px;text-align:center;}\n.adawatweb h1{color:red}.adawatweb p{color:#579bd5}";
+  var default_html="<!--هذا مجرد نموذج للرمز البرمجي، إلصق الكود الحقيقي \n يمكنك تغيير لغة الرمز البرمجي من أعلى هذا الصندوق .-->\n\n<!DOCTYPE html> <html dir='rtl'> <head> <meta charset='utf-8'>\n\n<title>موقع أدوات ويب</title></head><body><h1>كيف يمكن إعادة ترتيب ملفات html </h1>\n<p>أداة إعادة ترتيب الرموز البرمجية لمحتويات الويب</p> <p id='demo'>\n\n</p><script> var text = 'إختبار إعادة تنظيم الملفات'; document.getElementById('demo').innerHTML = text;</script></body></html>\n<!--إضغط على زر تطبيق لمشاهدة مثال-->";
+var default_css = "/*===\nهذا مجرد نموذج للرمز البرمجي، إلصق الكود الحقيقي \n يمكنك تغيير لغة الرمز البرمجي من أعلى هذا الصندوق. \n===*/\n\n#adawatweb{border: 1px solid #eee;height:auto;padding:15px;text-align:center;}\n.adawatweb h1{color:red}.adawatweb p{color:#579bd5}\n/*إضغط على زر تطبيق لمشاهدة مثال*/";
   var default_text =
-    "// هذا مجرد نموذج للرمز البرمجي، إلصق الكود الحقيقي \n //يمكنك تغيير نوع الرمز البرمجي من أعلى هذا الصندوق .\n\nif ('this_is'==/an_example/){of_beautifier();}else{var a=b?(c%d):e[f];}";
+    "// هذا مجرد نموذج للرمز البرمجي، إلصق الكود الحقيقي \n //يمكنك تغيير لغة الرمز البرمجي من أعلى هذا الصندوق .\n\nif ('this_is'==/an_example/){of_beautifier();}else{var a=b?(c%d):e[f];}\n//إضغط على زر تطبيق لمشاهدة مثال";
   var textArea = $('#source')[0];
 		  if ($("#language").val() === "css"){
 			$('#source').val(default_css);
