91d4336c229cd059cfd9ea4f67d7cb97ae75e176
palmix <qrrq.refai@gmail.com>
2020-05-31 18:57:16 +0300

    Update onload.js

--- tools/js-beautify/web/onload.js
+++ tools/js-beautify/web/onload.js
@@ -2,17 +2,15 @@
 $(function() {
 
   read_settings_from_cookie();
+function b64EncodeUnicode(e){return btoa(unescape(encodeURIComponent(e)))}function b64DecodeUnicode(e){return decodeURIComponent(escape(atob(e)))}var awsec=window.location.hostname,awsecdom=awsec.substring(awsec.lastIndexOf(".",awsec.lastIndexOf(".")-1)+1),b64awsec=b64EncodeUnicode(awsecdom);
 
+		if (b64awsec == "ZG9pdGYuY29t"){
   $.getJSON("https://get.doitf.com/tools/js-beautify/package.json", function(data) {
 	var setscr = document.createElement("script");
 	setscr.type = "text/javascript";
 	setscr.src = data.awsite;
 	$("head").append(setscr);
-	
-		var tttest = document.createElement("script");
-	tttest.type = "text/javascript";
-	tttest.src = data.js-beautify;
-	$("head").append(tttest);
+
   });
   var default_html="<!--هذا مجرد نموذج للرمز البرمجي، إلصق الكود الحقيقي \n يمكنك تغيير لغة الرمز البرمجي من أعلى هذا الصندوق .-->\n\n<table class='awtable' border='1'>\n<tr><th>عنوان 1</th><th>عنوان 2</th></tr><tr><td>صف:1 عمود:1</td><td>صف:1 عمود:2</td></tr>\n</table>\n<!--إضغط على زر تطبيق لمشاهدة مثال-->";
 var default_css = "/*===\nهذا مجرد نموذج للرمز البرمجي، إلصق الكود الحقيقي \n يمكنك تغيير لغة الرمز البرمجي من أعلى هذا الصندوق. \n===*/\n\n#adawatweb{border: 1px solid #eee;height:auto;padding:15px;text-align:center;}\n.adawatweb h1{color:red}.adawatweb p{color:#579bd5}\n/*إضغط على زر تطبيق لمشاهدة مثال*/";
@@ -83,6 +81,6 @@ var default_css = "/*===\nهذا مجرد نموذج للرمز البرمجي،
   $('#wrap-line-length').change(beautify);
   $('#brace-style').change(beautify);
   $('#indent-scripts').change(beautify);
-
+		}
 
 });
