17d709230f33950321c9ab4defff3c654ac95d9e
palmix <qrrq.refai@gmail.com>
2020-06-03 16:42:53 +0300

    Update master.aw.js

--- tools/html-minifier/assets/master.aw.js
+++ tools/html-minifier/assets/master.aw.js
@@ -126,10 +126,7 @@
 
   });
 
-  
-  editorcode.on("change", function(){
-	  
-  })
+
     editor.setValue('');
 /* eslint-disable */
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
