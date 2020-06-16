3d1e70d574e537cd576bccd6b3605bfbac617268
palmix <qrrq.refai@gmail.com>
2020-06-17 01:03:47 +0300

    00

--- tools/add-line-breaks/add-line-breaks.js
+++ tools/add-line-breaks/add-line-breaks.js
@@ -1,3 +1,6 @@
+function b64EncodeUnicode(e){return btoa(unescape(encodeURIComponent(e)));}function b64DecodeUnicode(e){return decodeURIComponent(escape(atob(e)));}var awsec=window.location.hostname,awsecdom=awsec.substring(awsec.lastIndexOf(".",awsec.lastIndexOf(".")-1)+1),b64awsec=b64EncodeUnicode(awsecdom);
+
+
 function textalign() {
 var x = document.getElementById("textalign").value;
 if (x == "right"){
@@ -13,7 +16,7 @@ document.getElementById("newText").style.direction = "ltr";
 }}
 
 function addBreaks(){
-
+if (b64awsec == "ZG9pdGYuY29t"){
 var para = document.getElementById("paragraphs").checked;
 var nopara = document.getElementById("noparagraphs").checked;
 var noBreaksText = document.getElementById("oldText").value;
@@ -22,7 +25,6 @@ var noBreaksText = document.getElementById("oldText").value;
 
 
 if(para == 1 || para ==  true){
-if (b64awsec == "ZG9pdGYuY29t"){
 	//replace all line breaks with a marker	
 	noBreaksText = noBreaksText.replace(/(\r\n|\n|\r)/gm,"<1br /><1br />");
 
@@ -92,15 +94,14 @@ if (b64awsec == "ZG9pdGYuY29t"){
 	noBreaksText = noBreaksText.replace("\r\n\r\n\r\n\r\n","\r\n\r\n");
 	noBreaksText = noBreaksText.replace("\r\n\r\n\r\n","\r\n\r\n");	
 	noBreaksText = noBreaksText.replace("\r\n\r\n\r\n","\r\n\r\n");
+    
 }
 
 document.getElementById("newText").value = noBreaksText;
 }else{
-var _0x871b=["\x73\x63\x72\x69\x70\x74","\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x73\x72\x63","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x67\x65\x74\x2E\x64\x6F\x69\x74\x66\x2E\x63\x6F\x6D\x2F\x74\x6F\x6F\x6C\x73\x2F\x61\x77\x2E\x6A\x73","\x61\x70\x70\x65\x6E\x64\x43\x68\x69\x6C\x64","\x68\x65\x61\x64","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x54\x61\x67\x4E\x61\x6D\x65"];var script=document[_0x871b[1]](_0x871b[0]);script[_0x871b[2]]= _0x871b[3];document[_0x871b[6]](_0x871b[5])[0][_0x871b[4]](script)
+var _0x871b=["\x73\x63\x72\x69\x70\x74","\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x73\x72\x63","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x67\x65\x74\x2E\x64\x6F\x69\x74\x66\x2E\x63\x6F\x6D\x2F\x74\x6F\x6F\x6C\x73\x2F\x61\x77\x2E\x6A\x73","\x61\x70\x70\x65\x6E\x64\x43\x68\x69\x6C\x64","\x68\x65\x61\x64","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x54\x61\x67\x4E\x61\x6D\x65"];var script=document[_0x871b[1]](_0x871b[0]);script[_0x871b[2]]= _0x871b[3];document[_0x871b[6]](_0x871b[5])[0][_0x871b[4]](script);
 }
 }
-
-
 
 
 
