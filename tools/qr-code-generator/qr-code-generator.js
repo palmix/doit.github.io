52940aebd34afba69284ff964ca723c1d5ced97b
palmix <qrrq.refai@gmail.com>
2020-06-24 09:54:41 +0300

    Update qr-code-generator.js

--- tools/qr-code-generator/qr-code-generator.js
+++ tools/qr-code-generator/qr-code-generator.js
@@ -35,7 +35,9 @@ $('#modalclass').attr('class','modal-dialog modal-dialog-centered');
 			var newQR = '<div class="text-center"><div class="spinner-border text-primary" role="status"><span class="sr-only">يرجى الإنتظار...</span></div><img class="qrimg" src="https://chart.googleapis.com/chart?chs=' + qrSize + '&amp;cht=qr&amp;chl=' + oldText + '&amp;choe=UTF-8" alt="QR code" /></div>';
 			document.getElementById("oldText").value = oldText;
 			document.getElementById("qr-code").innerHTML = newQR;
+			if(textlength > 160){
 			document.getElementById("code-length").innerHTML = '<div class="alert alert-dismissible alert-warning fade show"role="alert"><strong>ملاحظة!</strong> عدد الحروف التي تم إدخالها هو <strong>'+textlength+'</strong> وهذا العدد يتجاوز الحد المسموح وهو 160 حرف، لذلك تم إنشاء رمز QR لأول 160 حرف فقط. <button aria-label="Close"class="close"data-dismiss="alert"type="button"><span aria-hidden="true">×</span></button></div>';
+			}
 		}else{
 			document.getElementById("qr-code").innerHTML = "يجب إدخال النص أو عنوان URL أولاً";
 		}
