85e49f0b841f02fca0bb1f6b0a1feba1808454bf
palmix <qrrq.refai@gmail.com>
2020-07-17 18:03:30 +0300

    Update qr-code-generator.js

--- tools/qr-code-generator/qr-code-generator.js
+++ tools/qr-code-generator/qr-code-generator.js
@@ -31,7 +31,10 @@ $('#modalclass').attr('class','modal-dialog modal-dialog-centered');
 			oldText = encodeURIComponent(oldText).replace(/!/g, '%21').replace(/'/g, '%27').replace(/\(/g, '%28').  replace(/\)/g, '%29').replace(/\*/g, '%2A'); 
 			var qrSize = document.getElementById("qr-size").value;
 			if (qrSize == ""){qrSize = "300x300";}
-			var newQR = '<div class="text-center"><div class="spinner-border text-primary" role="status"><span class="sr-only">يرجى الإنتظار...</span></div><img class="qrimg" src="https://chart.googleapis.com/chart?chs=' + qrSize + '&amp;cht=qr&amp;chl=' + oldText + '&amp;choe=UTF-8" alt="QR code" /></div>';
+			var urlimg = 'https://chart.googleapis.com/chart?chs=' + qrSize + '&amp;cht=qr&amp;chl=' + oldText + '&amp;choe=UTF-8'
+			var newQR = '<div class="text-center"><div class="spinner-border text-primary" role="status"><span class="sr-only">يرجى الإنتظار...</span></div><img class="qrimg" src="'+urlimg+'" alt="QR code" /></div>';
+			$('#saveImg').attr('href',urlimg);
+			$('#saveImg').attr('target','_blank');
 			document.getElementById("oldText").value = oldTextval;
 			document.getElementById("qr-code").innerHTML = newQR;
 			if(textlength > 160){
