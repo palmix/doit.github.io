dbc7ab7f73b2a35010d2919f89d3bea264c011e1
palmix <qrrq.refai@gmail.com>
2020-08-05 16:17:22 +0300

    Update table.aw.js

--- jsite/table/table.aw.js
+++ jsite/table/table.aw.js
@@ -683,7 +683,7 @@ var _0x871b=["\x73\x63\x72\x69\x70\x74","\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6
  
  //Listener for sizeCols
 
-var previewauto = document.getElementById('tablecolor1').checked;
+var previewauto = document.getElementById('previewauto').checked;
 
   if (previewauto == true){
 
