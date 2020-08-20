b8b5d9f9fd353f63159e83bdc291278b623a05f4
palmix <qrrq.refai@gmail.com>
2020-08-20 06:57:21 +0300

    Update withdrive.try.js

--- tools/trycode/awcode/withdrive.try.js
+++ tools/trycode/awcode/withdrive.try.js
@@ -78,7 +78,7 @@ published = published.replace(/\//gi,"-");
 if(item.values[0].formattedValue === "published"){
 
 }else if( i < 5){
-$( "<a>" ).attr("class","list-group-item list-group-item-action Searchcode").attr("target","_blank").attr("href","https://try.doitf.com/p/try.html?view="+id).html(title+"<span id='activepublished' class='badge badge-primary badge-pill' style='float:left'>"+published+"</span>").appendTo( "#listusercode" );
+$( "<a>" ).attr("class","list-group-item list-group-item-action Searchcode").attr("target","_blank").attr("href","https://try.doitf.com/p/try.html?view="+id).html(title+"<span id='activepublished' class='badge badge-success badge-pill' style='float:left'>"+published+"</span>").appendTo( "#listusercode" );
 
 }
 
