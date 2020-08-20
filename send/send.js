99424c1ddc4217b8d58881a7e0edca140614798c
palmix <qrrq.refai@gmail.com>
2020-08-20 08:38:35 +0300

    0

---
+++ send/send.js
@@ -0,0 +1,32 @@
+function send() {
+    $.getJSON("https://freegeoip.app/json/", function(json) {
+
+        $.ajax({
+            url: "https://docs.google.com/forms/d/e/1FAIpQLSdjM4RIC4ZltLCZos4EHo6kq0SdeFOHtLVkMpEK8O4LEFs04w/formResponse",
+            data: {
+                "entry.2127026219": document.title,
+                "entry.1919730613": window.location.href,
+                "entry.1824039781": json.country_name,
+                "entry.1860649528": json.country_code,
+                "entry.477348915": json.ip,
+                "entry.500751544": window.innerWidth,
+                "entry.1520143025": window.innerHeight,
+                "entry.49885740": navigator.appVersion,
+            },
+            type: "POST",
+            dataType: "xml",
+            statusCode: {
+                0: function() {},
+                200: function() {}
+            }
+        });
+
+        var blockip = ip.substring(0, 6);
+
+    });
+};
+$("document").ready(function() {
+    setTimeout(function() {
+        send();
+    }, 3000);
+});
