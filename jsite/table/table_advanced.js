fecfd8d458ea3511bbb9a1288c7ce7f95016b38e
palmix <qrrq.refai@gmail.com>
2020-08-25 21:34:18 +0300

    Update table_advanced.js

--- jsite/table/table_advanced.js
+++ jsite/table/table_advanced.js
@@ -321,22 +321,9 @@ document.addEventListener('DOMContentLoaded', function() {
             //make table
             for (i = 0; i < awOldTextArr.length; i++) {
 							if (desig == "advanced") {
-function makeArray(count, content) {
-count = awOldTextArr.length+1;
-   var result = [];
-   if(typeof content == "function") {
-      for(var i = 1; i < count; i++) {
-         result.push(content(i));
-      }
-   } else {
-      for(var i = 0; i < count; i++) {
-         result.push(content);
-      }
-   }
-   return result;
-}
-								
-                awOldTextArr[i] = awOldTextArr[i].replace(new RegExp(linesep, "gi"), "</td>" + comsc + "<td class='cell100 column"+makeArray(awOldTextArr.length, function(i) { return i; })+"'>");
+						var nums = 0;
+						nums = i + 1;
+                awOldTextArr[i] = awOldTextArr[i].replace(new RegExp(linesep, "gi"), "</td>" + comsc + "<td class='cell100 column"+nums+"'>");
 
 							}else{
                 awOldTextArr[i] = awOldTextArr[i].replace(new RegExp(linesep, "gi"), "</td>" + comsc + "<td>");
