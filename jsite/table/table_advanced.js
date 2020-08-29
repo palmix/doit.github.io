b55850f868fa1db90aa97616acdccedec266bc6f
palmix <qrrq.refai@gmail.com>
2020-08-29 03:47:03 +0300

    Update table_advanced.js

--- jsite/table/table_advanced.js
+++ jsite/table/table_advanced.js
@@ -1323,7 +1323,7 @@ if(ffhtf == true || ffctf == true){
                 var incode = newCodeedit.value;
 
                 newTable1.innerHTML = incode;
-                newTableStyle.innerHTML = '<style type="text/css">.table100 table{color:#EFEFEF;width:100%;border-width:1px;border-color:#A1A1A1;border-collapse:collapse;}.table100 th{font-family:initial;color:#EFEFEF;font-size:13px;background-color:#5F5F5F;border-width:1px;border-style:solid;border-color:#A1A1A1;padding: 8px;}.awtable tr{background-color:#F5F5F5;}.table100 td{font-size:12px;font-family:initial;border-width:1px;border-style:solid;border-color:#A1A1A1;padding:8px;color:#3E3E3E;}.table100 tr:hover,.table100 tr:nth-of-type(odd):hover{background-color:#A3A3A3;}.table100 tr:nth-of-type(odd){background-color:#CACACA;}'+columnnum+'</style>';
+                newTableStyle.innerHTML = '<style type="text/css">.table100 table{color:#fff;width:100%;border-width:1px;border-color:#0e0e0e;border-collapse:collapse}.table100 table th{font-family:initial;color:#fff;font-size:13px;background-color:#565656;border-width:1px;border-style:solid;border-color:#0e0e0e;padding:8px}.table100 table tr{background-color:#fff}.table100 table td{font-size:12px;font-family:initial;border-width:1px;border-style:solid;border-color:#0e0e0e;padding:8px;color:#3e3e3e}.table100 table tr:hover{background-color:#d1edff}.table100 table tr td:hover{background-color:#a8d7ff}'+columnnum+'</style>';
                 incode = incode.replace(' contenteditable="true"', '');
 
                 if (comsc.checked === true) {
@@ -1798,7 +1798,10 @@ $('#remove_columns').click(function() {
 
 
 
-
+$('#removetextftbtn').on("click", function() {
+$('#newTable1 table td').html("<br/>");
+makeTableCode();
+});
 
 
 
