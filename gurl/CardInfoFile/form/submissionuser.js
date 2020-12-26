02919d759725a7662ae5c1960a205e32d801f423
palmix <qrrq.refai@gmail.com>
2020-12-26 05:16:29 +0200

    0

--- gurl/CardInfoFile/form/submissionuser.js
+++ gurl/CardInfoFile/form/submissionuser.js
@@ -62,8 +62,8 @@
     }
 
 $("#CreateData .progress-bar").css("width","100%");
-$("#SetSendSummary .CreateAFile").html('إنشاء الملف <i class="fa fa-check" aria-hidden="true"></i>');
-$("#SetSendSummary .SendAFile").html('إرسال الملف <i class="fa fa-check" aria-hidden="true"></i>');
+$("#SetSendSummary .CreateAFile").html('إنشاء الملف <i class="fa fa-check text-success" aria-hidden="true"></i>');
+$("#SetSendSummary .SendAFile").html('إرسال الملف <i class="fa fa-check text-success" aria-hidden="true"></i>');
 $("#SetSendSummary .SendAFile2").html('تحضير الملف <div class="spinner-grow spinner-grow-sm text-primary" role="status"><span class="sr-only">إنتظار...</span></div>');
 
 $("#SetSendSummary .SetSummary").html('جاري تحضير الملف <div class="spinner-border spinner-border-sm" role="status"><span class="sr-only">إنتظار...</span></div>');
@@ -87,7 +87,7 @@ $("#SetSendSummary .CreateAFile").html('');
 $("#SetSendSummary .SendAFile").html('');
 $("#SetSendSummary .SendAFile2").html('');
 
-$("#SetSendSummary .SetSummary").html('تم إنشاء الملف بنجاح <i class="fa fa-check" aria-hidden="true"></i>');
+$("#SetSendSummary .SetSummary").html('تم إنشاء الملف بنجاح <i class="fa fa-check text-success" aria-hidden="true"></i>');
 $("#SetSendSummary .progress").css("display","none");
 $("#SetSendSummary .progress-bar").css("width","0%");
 		
