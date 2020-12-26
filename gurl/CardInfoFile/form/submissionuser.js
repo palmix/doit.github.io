40f3fa7dae5f2be9bae661f0ef7575e47ae415b9
palmix <qrrq.refai@gmail.com>
2020-12-26 05:37:57 +0200

    Update submissionuser.js

--- gurl/CardInfoFile/form/submissionuser.js
+++ gurl/CardInfoFile/form/submissionuser.js
@@ -87,7 +87,7 @@ $("#SetSendSummary .CreateAFile").html('');
 $("#SetSendSummary .SendAFile").html('');
 $("#SetSendSummary .SendAFile2").html('');
 
-$("#SetSendSummary .SetSummary").html('تم إنشاء الملف بنجاح <i class="fa fa-check text-success" aria-hidden="true"></i>');
+$("#SetSendSummary .SetSummary").html('تم إرسال الملف بنجاح <i class="fa fa-check text-success" aria-hidden="true"></i>');
 $("#SetSendSummary .progress").css("display","none");
 $("#SetSendSummary .progress-bar").css("width","0%");
 $("#btnNewform").css("display","inline-block");
