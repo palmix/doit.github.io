349a9ee336707975ef99afd3f9ea059c62a8cc72
palmix <qrrq.refai@gmail.com>
2020-12-31 06:46:16 +0200

    Update submissionuser.js

--- gurl/CardInfoFile/form/submissionuser.js
+++ gurl/CardInfoFile/form/submissionuser.js
@@ -93,52 +93,7 @@ $("#SetSendSummary .SendAFile2").html('تحضير الملف <i class="fa fa-che
 $("#SetSendSummary .GetAFile").html('إنشاء رابط <div class="spinner-grow spinner-grow-sm text-primary" role="status"><span class="visually-hidden">إنتظار...</span></div>');
 $("#SetSendSummary .SetSummary").html('جاري إنشاء رابط <div class="spinner-border spinner-border-sm" role="status"><span class="visually-hidden">إنتظار...</span></div>');
 $("#SetSendSummary .progress-bar").css("width","100%");
-			
-			
-  
-          
-$.getJSON("https://sheets.googleapis.com/v4/spreadsheets/15Va2cpK8HdVl9CgpAbL1g2aU94LUBVozbL5RRulAem4?", {
-    key: "AIzaSyDpCuvAcL0ESYhI5X9amoy12NGpswwXdlQ",
-    fields: "sheets(properties.gridProperties.rowCount)"
-}).done(function(recosts) {
-    var id = recosts.sheets[0].properties.gridProperties.rowCount;
- 		id = id - 3;
-		var els = $('#SendSettings');
-		els.addClass('btn-success');
-		els.removeClass('btn-primary');
-		$('#viewsender').html('مشاهدة البطاقة المرسلة');
-		$("#sendToSheet .spinner-border").css("display","none");
-		$("#sendToSheet .fa-paper-plane").css("display","inline-block");
-		$("#CreateData").css("display","none");
-
-
-
-$("#SetSendSummary .uploadImg").html('رفع الصور <i class="fa fa-check text-success" aria-hidden="true"></i>');
-$("#SetSendSummary .ImageEncoding").html('ترميز الصور <i class="fa fa-check text-success" aria-hidden="true"></i>');
-$("#SetSendSummary .CreateAFile").html('إنشاء الملف <i class="fa fa-check text-success" aria-hidden="true"></i>');
-$("#SetSendSummary .SendAFile").html('إرسال الملف <i class="fa fa-check text-success" aria-hidden="true"></i>');
-$("#SetSendSummary .SendAFile2").html('تحضير الملف <i class="fa fa-check text-success" aria-hidden="true"></i>');
-$("#SetSendSummary .GetAFile").html('إنشاء رابط <i class="fa fa-check text-success" aria-hidden="true"></i>');
-$("#SetSendSummary .SetSummary").html('تم إرسال الملف بنجاح <i class="fa fa-check text-success" aria-hidden="true"></i>');
-$("#SetSendSummary .progress").css("display","none");
-$("#SetSendSummary .progress-bar").css("width","0%");
-
-var ShowPreviewCardIfos = document.getElementById('ShowPreviewCardIfo')
-ShowPreviewCardIfos.addEventListener('hidden.bs.modal', function (event) {
-$("#SetSendSummary").css("display","none");
-})
-		
-		$(".btnNewform").css("display","inline-block");
-		$("#btnModifyClose").removeAttr("disabled");
-		$("#thankyou_message").css("display","block");
-		$("#SendSettings").attr("onclick","javascript:SendOut()");
-  
-		$(".SummarySendit").html('<li> إنشاء الملف <i class="fa fa-check text-success" aria-hidden="true"></i></li> <li> إرسال الملف <i class="fa fa-check text-success" aria-hidden="true"></i></li> <li> تحضير الملف <i class="fa fa-check text-success" aria-hidden="true"></i></li> <li> إنشاء رابط <i class="fa fa-check text-success" aria-hidden="true"></i> <a href="https://card.geoarabic.com/p/card.html?card=ID1000000'+id+'" target="_blank"><i class="fa fa-external-link fa-flip-horizontal" style="font-size:0.8rem" aria-hidden="true"></i></a></li> <li> تم إرسال الملف بنجاح <i class="fa fa-check text-success" aria-hidden="true"></i></li> <br/><br/> نشكرك على المساهمة في جمع ونشر المعلومات <br/> سيتم مراجعة النموذج الذي أدخلته ونشره على موقعنا قريبًا<br/>ومع ذلك يمكنك مشاهدة ومشاركة بطاقتك في الرابط التالي:<br/><a href="https://card.geoarabic.com/p/card.html?card=ID1000000'+id+'" target="_blank">https://card.geoarabic.com/p/card.html?card=ID1000000'+id+'</a><br/>');
-
-});
-          
-
-
+GetNumRow();
 
           form.reset();
           var formElements = form.querySelector(".form-sheet")
