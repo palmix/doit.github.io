be3027e11c500db2f7f80a80b7f9df2c0c6c32e2
palmix <qrrq.refai@gmail.com>
2020-12-31 09:28:27 +0200

    0

--- gurl/CardInfoFile/form/form-submission-handler.js
+++ gurl/CardInfoFile/form/form-submission-handler.js
@@ -69,7 +69,7 @@ SendAFile();
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
-			$("#UsersendToSheetHide1").click();
+			SendAFile2()
           form.reset();
           var formElements = form.querySelector(".form-sheet")
           if (formElements) {
