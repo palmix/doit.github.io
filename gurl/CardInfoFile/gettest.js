62620c27aa2920f2750d383dfd657ebbd88d2247
palmix <qrrq.refai@gmail.com>
2021-01-27 20:53:14 +0200

    Update gettest.js

--- gurl/CardInfoFile/gettest.js
+++ gurl/CardInfoFile/gettest.js
@@ -7,7 +7,7 @@ var getnumCards = $.query.get("card");
 if(ss == "/%d8%ad%d9%8a%d9%88%d8%a7%d9%86%d8%a7%d8%aa/%d9%84%d8%a7%d9%81%d9%82%d8%a7%d8%b1%d9%8a%d8%a7%d8%aa/"){
 var getnumCards1 = getnumCards + 1;
 var getlink = $('.entry .so-widget-sow-editor a:eq('+getnumCards1+')').attr('href');
-if(getnumCards1 <= 14){
+if(getnumCards1 <= 27){
 	
 window.location.href = getlink + "?card=" + getnumCards1;
 }else{
