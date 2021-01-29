b6cca910333e6da4e714ce4f959166a13978d99c
palmix <qrrq.refai@gmail.com>
2021-01-29 23:48:22 +0200

    Update gettest.js

--- gurl/CardInfoFile/nationalgeographic/gettest.js
+++ gurl/CardInfoFile/nationalgeographic/gettest.js
@@ -1280,7 +1280,7 @@ $('#g9').val(sizeimg);
 
 var textsize = $( ".fast-facts__facts__key" ).last().text();
 if(textsize.length > 1){
-  if(textsize == "SIZE RELATIVE TO A 6-FT MAN:"){
+  if(textsize == "SIZE RELATIVE TO A 6-FT MAN:" || textsize == "SIZE RELATIVE TO A 6-FT MAN: "){
   $('#g9_1').val(3);
   }
 }
