b49271e546e29b23871b14377d2e6ca6c0bf058b
palmix <qrrq.refai@gmail.com>
2021-01-30 03:27:59 +0200

    Update gettest.js

--- gurl/CardInfoFile/nationalgeographic/gettest.js
+++ gurl/CardInfoFile/nationalgeographic/gettest.js
@@ -1423,14 +1423,14 @@ function gotonext(){
 
 
 var getnumCardsnext = $.query.get("card");
-var getnumCards1 = getnumCardsnext + 1;
+var getnumCards1next = getnumCardsnext + 1;
 var getnamenext = $("#nextcardname").val();
 var s = getnamenext.toLowerCase();
 var namenext = s.replace(/ /ig,'-');
 var namesnext =  s.substring(0,1);
 var numsnext = getnumCards1;
   
-if(getnumCards1 <= 387){
+if(getnumCards1next <= 387){
 var sc = 'https://www.nationalgeographic.com/animals/mammals/';
   sc += namesnext+'/';
   sc +=namenext;
