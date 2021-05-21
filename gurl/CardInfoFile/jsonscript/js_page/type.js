843933a01d435ad281744dac8f35251f67eb85a0
palmix <qrrq.refai@gmail.com>
2021-05-21 13:39:55 +0300

    Update type.js

--- gurl/CardInfoFile/jsonscript/js_page/type.js
+++ gurl/CardInfoFile/jsonscript/js_page/type.js
@@ -3,7 +3,7 @@
     alt: "json",
     fields: "sheets(data.rowData.values.formattedValue)"
     }).catch(function(error) {
-     console.log(error.status)
+
    $(".setSheetHideSite").removeClass("setSheetHideSite");
    $("#cardslistall").html('<div class="alert alert-danger d-flex align-items-center" role="alert"><i class="fa fa-info-circle mx-2" aria-hidden="true"></i><div>حدث خطأ ما! يرجى إعادة المحاولة، رمز الخطأ: '+error.status+'</div></div>');
      
